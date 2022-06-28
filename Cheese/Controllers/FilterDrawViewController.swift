0b2af2787f71240712caa1dd19398fc869aeb457
Y00ujin <yujingim43@gmail.com>
2022-06-28 17:28:59 +0900

    [ADD] LaunchScreen.storyboard add

--- Cheese/Controllers/FilterDrawViewController.swift
+++ Cheese/Controllers/FilterDrawViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class x: UIViewController {
+class FilterDrawViewController: UIViewController {
     
     // MARK: - Properties
     private let size = UIScreen.main.bounds
--- Cheese/Controllers/FilterWriteViewController.swift
+++ Cheese/Controllers/FilterWriteViewController.swift
@@ -14,7 +14,50 @@ class FilterWriteViewController: UIViewController {
         $0.backButton.addTarget(self, action: #selector(backButtonClicked(sender:)), for: .touchUpInside)
         $0.filterSaveButton.addTarget(self, action: #selector(filterSaveButtonClicked(sender:)), for: .touchUpInside)
     }
+    
     private let filterBottomView = FilterBottomView()
+    
+    private let inputTextField = UITextField().then{
+        $0.placeholder = "글씨를 입력해주세요."
+        $0.backgroundColor = UIColor.rgb(r: 245, g: 245, b: 245)
+        $0.textAlignment = .center
+        $0.layer.cornerRadius = 10
+        $0.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 13)
+    }
+    
+    private var textLabel = UILabel().then{
+        $0.text = ""
+        $0.isHidden = true
+        $0.textColor = UIColor.black
+        $0.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 13)
+    }
+    
+    private let createTextButton = UIButton().then{
+        $0.setTitle("글씨 생성하기", for: .normal)
+        $0.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 11)
+        $0.layer.cornerRadius = 10
+        $0.setTitleColor(UIColor.rgb(r: 50, g: 50, b: 50), for: .normal)
+        $0.backgroundColor = UIColor(red: 238/255, green: 207/255, blue: 98/255, alpha: 1)
+        $0.addTarget(self, action: #selector(createTextButtonClicked(sender:)), for: .touchUpInside)
+    }
+    
+    private let upSizeButton = UIButton().then{
+        $0.setTitle("T", for: .normal)
+        $0.setTitleColor(UIColor.white, for: .normal)
+        $0.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 14)
+        $0.layer.cornerRadius = 10
+        $0.backgroundColor = UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1)
+        $0.addTarget(self, action: #selector(upSizeButtonClicked(sender:)), for: .touchUpInside)
+    }
+    
+    private let downSizeButton = UIButton().then{
+        $0.setTitle("T", for: .normal)
+        $0.setTitleColor(UIColor.white, for: .normal)
+        $0.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 11)
+        $0.layer.cornerRadius = 10
+        $0.backgroundColor = UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1)
+        $0.addTarget(self, action: #selector(downSizeButtonClicked(sender:)), for: .touchUpInside)
+    }
 
     // MARK: - LifeCycles
     override func viewDidLoad() {
@@ -33,7 +76,9 @@ class FilterWriteViewController: UIViewController {
     
     // MARK: - addView Function
     private func addView(){
-        [filterHeaderView, filterBottomView].forEach{ view.addSubview($0) }
+        [filterHeaderView, filterBottomView, inputTextField, createTextButton, textLabel, upSizeButton, downSizeButton].forEach{ view.addSubview($0) }
+        
+        [upSizeButton, downSizeButton].forEach{ filterBottomView.addSubview($0) }
     }
     
     // MARK: - addLocation()
@@ -48,6 +93,36 @@ class FilterWriteViewController: UIViewController {
             $0.bottom.equalTo(view.safeAreaLayoutGuide)
             $0.left.right.equalToSuperview()
             $0.height.equalToSuperview().dividedBy(12.49)
+        }
+        
+        inputTextField.snp.makeConstraints {
+            $0.width.equalToSuperview().dividedBy(1.4)
+            $0.center.equalToSuperview()
+            $0.height.equalToSuperview().dividedBy(10)
+        }
+        
+        createTextButton.snp.makeConstraints{
+            $0.top.equalTo(inputTextField.snp.bottom).offset(view.frame.height/120)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(inputTextField).dividedBy(2.4)
+            $0.width.equalTo(inputTextField).dividedBy(2.5)
+        }
+        
+        textLabel.snp.makeConstraints{
+            $0.center.equalToSuperview()
+        }
+        
+        upSizeButton.snp.makeConstraints{
+            $0.height.equalTo(createTextButton)
+            $0.width.equalTo(createTextButton.snp.height)
+            $0.centerY.equalTo(filterBottomView)
+            $0.left.equalToSuperview().offset(self.view.frame.width/1.9)
+        }
+        
+        downSizeButton.snp.makeConstraints{
+            $0.height.width.equalTo(upSizeButton)
+            $0.centerY.equalTo(filterBottomView)
+            $0.right.equalToSuperview().offset(-self.view.frame.width/1.9)
         }
     }
     
@@ -64,5 +139,20 @@ class FilterWriteViewController: UIViewController {
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.7) {
             self.navigationController?.popToRootViewController(animated: true)
         }
+    }
+    
+    @objc private func createTextButtonClicked(sender: UIButton){
+        [createTextButton, inputTextField].forEach{ $0.isHidden = true }
+        
+        textLabel.text = inputTextField.text
+        textLabel.isHidden = false
+    }
+    
+    @objc private func upSizeButtonClicked(sender: UIButton){
+        textLabel.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 20)
+    }
+    
+    @objc private func downSizeButtonClicked(sender: UIButton){
+        textLabel.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 12)
     }
 }
